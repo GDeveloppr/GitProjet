facb1daacd5b01d2b5e5e93bcf5949e4691c7e3a
denaneghilesinfo@gmail.com <denaneghilesinfo@gmail.com>
2017-07-28 17:29:26 +0100

    je vien de rajouter une variable qui va contenir tous le messa
    q

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-alert("hello world je me presente je suis monsieur GDeveloppeur");
+var helloText = "coucouc tous le monde je me present je suis denane ghiles né le 04/10/1994 je suis un fanne de la programmation en tous genre et cela depuis que j'etais tous petit "
+alert(helloText);
