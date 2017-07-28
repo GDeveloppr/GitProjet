e08c874f9468e8dc9ce48324b2e22988e2259bc1
denaneghilesinfo@gmail.com <denaneghilesinfo@gmail.com>
2017-07-28 17:48:14 +0100

    je vien de faire une erreur explicitement

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var helloText = "coucouc tous le monde je me present je suis denane ghiles né le 04/10/1994 je suis un fanne de la programmation en tous genre et cela depuis que j'etais tous petit "
-alert(helloText);
+alert(helloText
